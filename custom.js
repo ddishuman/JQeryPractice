61891cf09ae321e4e893c164f36f098971918f39
Martin Wang <alpa.wong@gmail.com>
2019-06-03 00:39:18 +0800

    Mouse leave element

--- custom.js
+++ custom.js
@@ -4,6 +4,12 @@ $(document).ready( function()
 {
   $("div").mouseenter(function(){
     console.log("Enter mouse");
+    $(this).css("color", "red");
+  });
+
+  $("div").mouseleave(function(){
+    console.log("Exited div");
+    $(this).css("color", "black");
   });
 
   $("span").mouseenter(function(){
