c918d20a6e86cf8501a38d4e65e3211d05e2ca21
Martin Wang <alpa.wong@gmail.com>
2019-06-03 13:44:40 +0800

    Adding elements using Append, prepend, after, before

--- custom.js
+++ custom.js
@@ -2,31 +2,19 @@
 
 $(document).ready( function()
 {
-  var width = $("#RedBox").width();
-  var height = $("#RedBox").height();
+  $("#AppendButton").click(function(){
+    $("#EpicDiv").append("<b>Append</b>");
+  });
 
-  var innerwidth = $("#RedBox").innerWidth();
-  var innerheight = $("#RedBox").innerHeight();
+  $("#PrependButton").click(function(){
+    $("#EpicDiv").prepend("<b>Prepend</b>");
+  });
 
-  var outerwidth = $("#RedBox").outerWidth();
-  var outerheight = $("#RedBox").outerHeight();
+  $("#AfterButton").click(function(){
+    $("#EpicDiv").after("<b>After</b>");
+  });
 
-  var outerwidthWithMargin = $("#RedBox").outerWidth(true);
-  var outerheightWithMargin = $("#RedBox").outerHeight(true);
-
-  console.log("width: " + width);
-  console.log("height: "+ height);
-
-  console.log("Inner width: " + innerwidth);
-  console.log("Inner height: "+ innerheight);
-
-  console.log("Outer width: " + outerwidth);
-  console.log("Outer height: "+ outerheight);
-
-  console.log("Outer width with margin: " + outerwidthWithMargin);
-  console.log("Outer height with margin: "+ outerheightWithMargin);
-
-  $("#ClickMe").click(function(){
-    $("#RedBox").width(400);
+  $("#BeforeButton").click(function(){
+    $("#EpicDiv").before("<b>Before</b>");
   });
 });
