e4b1b31a0fb19051632bd48da2ef57986fbaebaa
Martin Wang <alpa.wong@gmail.com>
2019-06-03 08:40:39 +0800

    Mouse up

--- custom.js
+++ custom.js
@@ -5,7 +5,12 @@ $(document).ready( function()
   $("div").click(function(){
     console.log("Clicked div");
   });
+
   $("div").mousedown(function(){
     console.log("Mouse down");
+  });
+
+  $("div").mouseup(function(){
+    console.log("Mouse up");
   });
 });
