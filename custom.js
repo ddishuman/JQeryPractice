6348abc856ff26ef06ab093d68636126a1ad5ef6
Martin Wang <alpa.wong@gmail.com>
2019-06-03 14:19:55 +0800

    Parent element

--- custom.js
+++ custom.js
@@ -2,7 +2,7 @@
 
 $(document).ready( function()
 {
-  //$("div").children().addClass("red");
-  $("div").children(".hello").addClass("red");
+  //$("#Hello").parent().addClass("red");
+  $("#Hello").parents().addClass("red");
 
 });
