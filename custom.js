ae55a5a545fc4e0dd7e7ee5ea51ee419d67c64c9
Martin Wang <alpa.wong@gmail.com>
2019-06-03 20:16:41 +0800

    Number checks

--- custom.js
+++ custom.js
@@ -2,7 +2,9 @@
 
 $(document).ready( function()
 {
-  var time = $.now();
+  console.log($.isNumeric(6));
+  console.log($.isNumeric("Hello"));
+  console.log($.isNumeric(-566));
+  console.log($.isNumeric("-566"));
 
-  console.log(time);
 });
