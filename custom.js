43bb130cef8f312ce7fae6161f4dc1af0bf5d242
Martin Wang <alpa.wong@gmail.com>
2019-06-03 13:37:00 +0800

    Set Element dimensions

--- custom.js
+++ custom.js
@@ -2,15 +2,31 @@
 
 $(document).ready( function()
 {
-  $("#AddClassButton").click(function(){
-    $("div").addClass("blue");
-  });
+  var width = $("#RedBox").width();
+  var height = $("#RedBox").height();
 
-  $("#RemoveClassButton").click(function(){
-    $("div").removeClass("blue");
-  });
+  var innerwidth = $("#RedBox").innerWidth();
+  var innerheight = $("#RedBox").innerHeight();
 
-  $("#ToggleClassButton").click(function(){
-    $("div").toggleClass("blue");
+  var outerwidth = $("#RedBox").outerWidth();
+  var outerheight = $("#RedBox").outerHeight();
+
+  var outerwidthWithMargin = $("#RedBox").outerWidth(true);
+  var outerheightWithMargin = $("#RedBox").outerHeight(true);
+
+  console.log("width: " + width);
+  console.log("height: "+ height);
+
+  console.log("Inner width: " + innerwidth);
+  console.log("Inner height: "+ innerheight);
+
+  console.log("Outer width: " + outerwidth);
+  console.log("Outer height: "+ outerheight);
+
+  console.log("Outer width with margin: " + outerwidthWithMargin);
+  console.log("Outer height with margin: "+ outerheightWithMargin);
+
+  $("#ClickMe").click(function(){
+    $("#RedBox").width(400);
   });
 });
